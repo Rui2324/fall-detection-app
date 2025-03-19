25dd26235191f48d9cca68ae2c830a4435a0ba6e
Gustavo <1gmplopes2006@gmail.com>
2025-03-19 20:32:04 +0000

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+namespace fall_detection_2._0
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
